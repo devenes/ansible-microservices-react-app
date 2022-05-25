a39a6c7caca91c000cbc7b643ea16305faec0b91
Enes <66560757+devenes@users.noreply.github.com>
2022-05-25 16:33:47 +0300

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,15 @@
 output "control_node_public_ip" {
-  value = aws_instance.control_node.public_ip
+  value = aws_instance.control_node.*.public_ip
 }
 
 output "control_node_private_ip" {
   value = aws_instance.control_node.*.private_ip
+}
+
+output "managed_node_public_ip" {
+  value = aws_instance.managed_node.*.public_ip
+}
+
+output "managed_node_private_ip" {
+  value = aws_instance.managed_node.*.private_ip
 }
