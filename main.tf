7ebabcf6863d1c5e756934e47b14993d34ce8df3
Enes <66560757+devenes@users.noreply.github.com>
2022-05-31 01:06:14 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -60,6 +60,12 @@ resource "aws_security_group" "tf-sec-gr" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
   ingress {
     from_port   = 5000
